49e5673ad1f2a25e574f42a24ba27cb198c07dfc
Cody Romano <codyromano@gmail.com>
2024-08-25 15:13:03 -0700

    Simplify the code for the little man

--- scenes/human.gd
+++ scenes/human.gd
@@ -1,16 +1,9 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+# const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta):
-	$HumanAnimatedSprite2D.flip_h = Input.is_action_pressed("move_left")
-		
-	if Input.is_action_pressed("move_right") || Input.is_action_pressed("move_left"):
-		$HumanAnimatedSprite2D.play("run_right")
-	else:
-		$HumanAnimatedSprite2D.play("idle")
-		
 	## TODO: Add gravity here if you want it
 	# Add the gravity.
 	# if not is_on_floor():
@@ -18,8 +11,12 @@ func _physics_process(delta):
 		# velocity.y += gravity * delta
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	# Flip the "run right" animation when the character is running left
+	$HumanAnimatedSprite2D.flip_h = Input.is_action_pressed("move_left")
+	$HumanAnimatedSprite2D.play("run" if direction else "idle")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
